fe20e2593bf160b4da37f0c59cc43f6bbe47ec21
badungle <badung.le@adelaide.edu.au>
2016-09-20 10:15:40 +0930

    Add files via upload

--- meta-LPAm_plus.c
+++ meta-LPAm_plus.c
@@ -642,37 +642,41 @@ int main() {
     
     igraph_vector_init(&membership, 0); 
     
-    /* Zachary Karate club -- this is just a quick smoke test */
-    igraph_small(&g, 0, IGRAPH_UNDIRECTED, 
-                 0,  1,  0,  2,  0,  3,  0,  4,  0,  5,
-                 0,  6,  0,  7,  0,  8,  0, 10,  0, 11,
-                 0, 12,  0, 13,  0, 17,  0, 19,  0, 21,
-                 0, 31,  1,  2,  1,  3,  1,  7,  1, 13,
-                 1, 17,  1, 19,  1, 21,  1, 30,  2,  3,
-                 2,  7,  2,  8,  2,  9,  2, 13,  2, 27,
-                 2, 28,  2, 32,  3,  7,  3, 12,  3, 13,
-                 4,  6,  4, 10,  5,  6,  5, 10,  5, 16,
-                 6, 16,  8, 30,  8, 32,  8, 33,  9, 33,
-                 13, 33, 14, 32, 14, 33, 15, 32, 15, 33,
-                 18, 32, 18, 33, 19, 33, 20, 32, 20, 33,
-                 22, 32, 22, 33, 23, 25, 23, 27, 23, 29,
-                 23, 32, 23, 33, 24, 25, 24, 27, 24, 31,
-                 25, 31, 26, 29, 26, 33, 27, 33, 28, 31,
-                 28, 33, 29, 32, 29, 33, 30, 32, 30, 33,
-                 31, 32, 31, 33, 32, 33,
-                 -1);    
+    // test the modularity found by meta-LPAm+ and the Louvain method on the dolphins network
+    FILE *fin;
+    fin = fopen("data//dolphins.GraphML", "r");
+    igraph_read_graph_graphml(&g, fin, 0);
+    fclose(fin);   
     
     metaLPAm_plus(&g, 0, &membership, &modularity);
-    
-    printf("Modularity:  %.4f\n", modularity);
-    printf("Membership: ");    
+    printf("Modularity by meta-LPAm+:  %.4f\n", modularity);
+    printf("Membership by meta-LPAm+: \n");    
     for (i=0; i<igraph_vector_size(&membership); i++) {
          printf("%li ", (long int)VECTOR(membership)[i]);
     } 
+    printf("\n\n");
             
+    igraph_vector_t modularities;
+    igraph_matrix_t memberships;  
+    igraph_vector_init(&modularities,0);
+    igraph_matrix_init(&memberships,0,0);  
+    
+    igraph_community_multilevel(&g, 0, &membership, &memberships, &modularities);
+    int j=igraph_vector_which_max(&modularities);
+    modularity = VECTOR(modularities)[j];
+                
+    printf("Modularity by the Louvain method:  %.4f\n", modularity);
+    printf("Membership by the Louvain method: \n");    
+    for (i=0; i<igraph_vector_size(&membership); i++) {
+         printf("%li ", (long int)VECTOR(membership)[i]);
+    } 
+    printf("\n");
+    
+    igraph_matrix_destroy(&memberships);
+    igraph_vector_destroy(&modularities);    
+    igraph_vector_destroy(&membership);      
     igraph_destroy(&g);    
-    igraph_vector_destroy(&membership);  
-    IGRAPH_FINALLY_CLEAN(2);  
+    IGRAPH_FINALLY_CLEAN(3);  
     
     return 0;
 }
